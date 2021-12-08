461ff97c29b43a343759e088eb53f199167b9bf4
nickforbizz <nwaruingi@kemri-wellcome.org>
2021-12-08 13:44:45 +0300

    Router Product Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import NLink from 'next/link'
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core'
 import Layout from '../components/Layout'
 import data from '../utils/data'
@@ -16,12 +17,14 @@ export default function Home() {
         {data.products.map((item, i) => (
           <Grid item md={4} key={i}>
               <Card>
-                <CardActionArea>
-                  <CardMedia component='img' image={item.image} title={item.title}></CardMedia>
-                  <CardContent>
-                    <Typography>{item.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NLink href={`/product/${item.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia component='img' image={item.image} title={item.title}></CardMedia>
+                    <CardContent>
+                      <Typography>{item.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NLink>
                 <CardActions>
                   <Typography>$ {item.price} </Typography>
                   <Button size='small' color='primary'>Add To Cart</Button>
